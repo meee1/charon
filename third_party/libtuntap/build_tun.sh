e08a88e4d18683e269d8cdd6895154dee3c5ebf1
Michael Oborne <mich146@hotmail.com>
2024-01-12 16:36:21 +1100

    fix tuntap.a

--- third_party/libtuntap/build_tun.sh
+++ third_party/libtuntap/build_tun.sh
@@ -1,6 +1,6 @@
 rm *.o
-arm-xilinx-linux-gnueabi-gcc  -c -O3 -std=c99 -I../../../buildroot/output/host/arm-buildroot-linux-gnueabi/sysroot/usr/include/ --sysroot=../../../buildroot/output/host/arm-buildroot-linux-gnueabi/sysroot/ tuntap.c
-arm-xilinx-linux-gnueabi-gcc  -c -O3 -std=c99 -I../../../buildroot/output/host/arm-buildroot-linux-gnueabi/sysroot/usr/include/ --sysroot=../../../buildroot/output/host/arm-buildroot-linux-gnueabi/sysroot/ tuntap_log.c
-arm-xilinx-linux-gnueabi-gcc  -c -O3 -std=c99 -I../../../buildroot/output/host/arm-buildroot-linux-gnueabi/sysroot/usr/include/ --sysroot=../../../buildroot/output/host/arm-buildroot-linux-gnueabi/sysroot/ tuntap-unix.c
-arm-xilinx-linux-gnueabi-gcc  -c -O3 -std=c99 -I../../../buildroot/output/host/arm-buildroot-linux-gnueabi/sysroot/usr/include/ --sysroot=../../../buildroot/output/host/arm-buildroot-linux-gnueabi/sysroot/ tuntap-unix-linux.c
-arm-xilinx-linux-gnueabi-ar rcs libtuntap.a *.o
+arm-linux-gnueabihf-gcc  -c -O3 -std=c99 -I../../../buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/ --sysroot=../../../buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/ tuntap.c
+arm-linux-gnueabihf-gcc  -c -O3 -std=c99 -I../../../buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/ --sysroot=../../../buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/ tuntap_log.c
+arm-linux-gnueabihf-gcc  -c -O3 -std=c99 -I../../../buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/ --sysroot=../../../buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/ tuntap-unix.c
+arm-linux-gnueabihf-gcc  -c -O3 -std=c99 -I../../../buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/usr/include/ --sysroot=../../../buildroot/output/host/arm-buildroot-linux-gnueabihf/sysroot/ tuntap-unix-linux.c
+arm-linux-gnueabihf-ar rcs libtuntap.a *.o
